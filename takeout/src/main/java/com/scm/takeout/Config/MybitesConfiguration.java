59ea6166bcfd6f6792b035b5a49fb17b4c77e3ef
songchangmeng <786742228@qq.com>
2022-07-02 10:22:33 +0900

    v1

---
+++ takeout/src/main/java/com/scm/takeout/Config/MybitesConfiguration.java
@@ -0,0 +1,16 @@
+package com.scm.takeout.Config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybitesConfiguration {
+    @Bean
+    MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor=new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
